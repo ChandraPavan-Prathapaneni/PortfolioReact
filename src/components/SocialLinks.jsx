071fce575a57d24b94ccf123cad957dfa57b9aac
ChandraPavan-Prathapaneni <prathapanenichandrapavan@gmail.com>
2024-09-11 15:33:58 -0500

    Mobile friendly web links tab

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -47,30 +47,51 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
-      <ul>
-        {links.map(({ id, child, href, style, download }) => (
-          <li
-            key={id}
-            className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-              " " +
-              style
-            }
-          >
+    <>
+      {/* Desktop version */}
+      <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+        <ul>
+          {links.map(({ id, child, href, style, download }) => (
+            <li
+              key={id}
+              className={
+                "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                " " +
+                style
+              }
+            >
+              <a
+                href={href}
+                className="flex justify-between items-center w-full text-white"
+                download={download}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {child}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Mobile version */}
+      <footer className="lg:hidden fixed bottom-0 left-0 w-full bg-gray-800 text-white">
+        <div className="flex justify-around py-4">
+          {links.map(({ id, child, href, download }) => (
             <a
+              key={id}
               href={href}
-              className="flex justify-between items-center w-full text-white"
+              className="flex flex-col items-center text-white"
               download={download}
               target="_blank"
               rel="noreferrer"
             >
               {child}
             </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </div>
+      </footer>
+    </>
   );
 };
 
